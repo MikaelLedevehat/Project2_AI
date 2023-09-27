87ace8f1c7fa5fe0fd061509f79cee079dfc8492
MikaelLedevehat <mikael.ledevehat@gmail.com>
2023-09-27 12:05:22 +0200

    2

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-dataSet = []
-c = []
-dataSetLenght = 1000
-
 def customFct(x):
     y = (x[0]*2.4 + x[1]*5.5)**2
     return round(y)
 
+dataSet = []
+c = []
+dataSetLenght = 1000
+max = customFct([10,10])
+
+
+
 def fillData(d):
     for i in range(dataSetLenght):
         d.append([random.random()*10,random.random()*10])
-        c.append(customFct(d[-1]))
+        c.append(round(customFct(d[-1])/max))
 
 
 fillData(dataSet)
-print(c)
 
 #plt.scatter(dataSet[:, 0], dataSet[:, 1], c=y, marker="o", cmap='plasma')
 scatter = plt.scatter([row[0] for row in dataSet], [row[1] for row in dataSet],c=c, label="data",cmap="plasma")
